e8c42aedcd0ed3aae35cae6f4a01a0131c0a90c2
StoicaO <125612945+StoicaO@users.noreply.github.com>
2023-08-26 01:04:47 +0300

    Add files via upload

--- airPlane.js
+++ airPlane.js
@@ -1,12 +1,12 @@
 
-let time = 0, minutes = 0, seconds = 0, intervalId = null, obstacleIntervalId = null;
+let time = 0, minutes = 0, seconds = 0, intervalId = null, addObstacleInterval = null;
 let start = document.getElementById("button1");
-let TEN = 10, SIXTY = 60, THOUSAND = 1000, twoThousand = 2000, EIGHTY = 80;
+let TEN = 10, SIXTY = 60, THOUSAND = 1000, twoThousand = 2000, fiveHUNDRED = 500;
 
 function increaseTime() {
     time += TEN;
     minutes = Math.floor(time / THOUSAND / SIXTY);
-    secunde = Math.floor(time / THOUSAND) % SIXTY;
+    seconds = Math.floor(time / THOUSAND) % SIXTY;
     document.getElementById("time").innerHTML = minutes + ':' + seconds;
 }
 
@@ -17,55 +17,66 @@ function generatePlane() {
     planeDiv.classList.add('plane');
     document.getElementById("board").appendChild(planeDiv); 
     document.onkeydown = detectKey;
-    obstacleIntervalId = setInterval(generateObstacole, twoThousand);   
+    startAddingObstacles();  
 }
 
-let planeLeft = 150, planeTop = 405, boardWidth = 440, hundred = 100;
+let planeLeft = 150, planeTop = 405, boardWidth = 440, HUNDRED = 100;
 
 function detectKey(e) {
     let planeDiv = document.querySelector(".plane");
     if (e.keyCode == '37' && planeLeft > 0) {
         planeLeft -= TEN;
-    }
-    else if (e.keyCode == '39' && planeLeft < boardWidth - hundred) {
+    } else if (e.keyCode == '39' && planeLeft < boardWidth - HUNDRED) {
         planeLeft += TEN;
     }
-        if (planeDiv) {
+    if (planeDiv) {
         planeDiv.style.top = planeTop + "px";
         planeDiv.style.left = planeLeft + "px";
     }
 }
 
 let avoidedObstacle = 0;
-function generateObstacole() {
-    let obstacle = document.createElement("div");
-    obstacle.classList.add('obs');
-    let  randomLeft = Math.floor((Math.random() * EIGHTY) + (hundred * 3)) +  "px";
-    obstacle.style.left = randomLeft + "px";
-    document.getElementById("board").appendChild(obstacle);
-    let moveStoneInterval = setInterval(function() {
-        moveStone(obstacle, moveStoneInterval);
-    }, 1000); 
-}
+let addObstacle = [15, 100, 150, 250, 300]; 
+let addObstacleIndex = 0; 
 
-function moveStone(obstacle, interval) {
-    let positionObs = obstacle.getBoundingClientRect();  
-    if (positionObs.top >= 0 && positionObs.top < (THOUSAND / 2)) {
+function generateObstacole() {
+    if (addObstacleIndex < addObstacle.length) {
+        let obstacle = document.createElement("div");
+        obstacle.classList.add('obs');
+        obstacle.style.left = addObstacle[addObstacleIndex] + "px";
+        document.getElementById("board").appendChild(obstacle);
+        ++addObstacleIndex;
+        return obstacle;
+    } else {
+        addObstacleIndex = 0;
+    }
+}  
+
+function moveObstacle(obstacle) {
+    let moveInterval = setInterval(function () {
+        let positionObs = obstacle.getBoundingClientRect();
         let newPosition = positionObs.top + 1;
         obstacle.style.top = newPosition + "px";
         if (checkCollision(document.querySelector(".plane"), obstacle)) {
-            clearInterval(interval);
+            clearInterval(moveInterval);
             obstacle.remove();
+            clearInterval(addObstacleInterval);
             gameOver();
             return; 
-        } console.log(obstacle.style.top);
-         if (obstacle.style.top > (400 + "px") && obstacle.style.top <= (500+ "px")) {        
+        } 
+        if (parseInt(obstacle.style.top) > (fiveHUNDRED - HUNDRED)  && parseInt(obstacle.style.top) <= fiveHUNDRED) {        
             ++avoidedObstacle;
+            obstacle.remove();
+            clearInterval(moveInterval); 
         }
-        
-    } else {
-        obstacle.style.top = "0px";
-    }   
+    }, fiveHUNDRED);
+}
+
+function startAddingObstacles() {
+    addObstacleInterval = setInterval(function () {
+        let obstacle = generateObstacole();
+        moveObstacle(obstacle);
+    }, twoThousand);     
 }
 
 function checkCollision(plane, obstacle) {
@@ -77,21 +88,17 @@ function checkCollision(plane, obstacle) {
         positionObs.top < positionPlane.bottom ) {
         return true;
     }
+    return false;
 }
 
 function gameOver() {
     clearInterval(intervalId);
-    intervalId = null;
-    clearInterval(obstacleIntervalId); 
-    obstacleIntervalId = null; 
     let image = document.createElement("img");
     image.classList.add('img');
     document.getElementById("board").appendChild(image);     
-    document.getElementById("game-over").innerHTML = "Avoided Obstacle " + avoidedObstacle ;
+    document.getElementById("game-over").innerHTML = "Avoided Obstacle " + avoidedObstacle;
 }
 
 function resetGame() {  
     location.reload();
 }
-
-
